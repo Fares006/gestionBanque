2968fc7cbfd2bf7104c0a335393ed44a2d93e7e0
Fares006 <faresmoussa06@gmail.com>
2025-03-26 16:14:44 +0100

    Gestion compte, budget

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@
 # --Imports-- #
 import datetime
 import tkinter as tk
+from pprint import pprint
 from tkinter import messagebox
-
 
 # --Constantes-- #
 CLE_CRYPTAGE = 23
@@ -232,7 +232,7 @@ def login() -> tuple:
     return False, identifiant
 
 
-def calcul_solde(lst_cpt: list, lst_ope: list) -> float:
+def calcul_solde(lst_cpt: list, lst_ope: list, courant=False) -> float:
     """
     Calcule le solde d'un utilisateur grâce à la liste des opérations associées au compte.
 
@@ -242,12 +242,14 @@ def calcul_solde(lst_cpt: list, lst_ope: list) -> float:
     Returns:
         le montant présent sur le compte
     """
-    print("Faites le choix du compte : ")
-    for i in range(len(lst_cpt)):
-        print(f'{i+1}. {lst_cpt[i]}')
-    choix = int(input("Choisissez le compte pour calculer son solde : ")) - 1   # On enlève le 1 de l'affichage
-    while choix not in range(len(lst_cpt)):
-        choix = int(input("Choisissez le compte pour calculer son solde : ")) - 1   # On enlève le 1 de l'affichage
+    choix = 0
+    if not courant:
+        print("Faites le choix du compte : ")
+        for i in range(len(lst_cpt)):
+            print(f'{i + 1}. {lst_cpt[i]}')
+        choix = int(input("Choisissez le compte pour calculer son solde : ")) - 1  # On enlève le 1 de l'affichage
+        while choix not in list(range(len(lst_cpt))):
+            choix = int(input("Choisissez le compte pour calculer son solde : ")) - 1  # On enlève le 1 de l'affichage
 
     compte_choisi = lst_cpt[choix]
     solde = 0
@@ -325,37 +327,44 @@ def identification():
         lst_cpt = import_comptes(chemin_fichier=f'../users/{identifiant}.txt', cle=dict_ident[identifiant][-1])
         lst_ope = import_operations(chemin_fichier=f'../users/{identifiant}.txt', cle=dict_ident[identifiant][-1])
         lst_bud = import_budgets(chemin_fichier=f'../users/{identifiant}.txt', cle=dict_ident[identifiant][-1])
-        print(lst_ope, lst_bud)
+
+        solde_courant = calcul_solde(lst_cpt, lst_ope, courant=True)
+        print(f"\n|-----Tableau de bord-----|\n"
+              f"| Bonjour {dict_ident[identifiant][1]} |\n"
+              f"| Vous avez {solde_courant}€ sur votre compte |")
+
         print("\nBienvenue. De quelle fonctionnalité avez-vous besoin ?")
-        print("0. Quitter")
-        print("1. Afficher le solde du compte")
-        print("2. Ajouter un compte")
-        print("3. Ajouter une opération")
-        print("4. Effectuer un virement")
-        print("5. Afficher les opérations d'un compte")
+        print("0. Quitter\n"
+              "1. Afficher le solde du compte\n"
+              "2. Ajouter un compte\n"
+              "3. Ajouter une opération\n"
+              "4. Effectuer un virement\n"
+              "5. Afficher les opérations d'un compte\n")
         choix = int(input("Votre choix : "))
         while choix != 0:
             match choix:
                 case 1:
-                    solde = calcul_solde(lst_ope)
-                    print(f"\n|-----Tableau de bord-----|\n"
-                          f"| Bonjour {dict_ident[identifiant][1]} |\n"
-                          f"| Vous avez {solde}€ sur votre compte |")
+                    solde = calcul_solde(lst_cpt, lst_ope)
+                    print(f"\n|-----Solde-----|\n"
+                          f"| Vous avez {solde}€ sur votre compte 635"
+                          f"1*£|")
+                    choix = int(input("Quelle fonctionnalité souhaitez-vous accéder ? : "))
                 case 2:
                     print("|-----Ajout de compte-----|")
                     ajout_compte()
                     print(f"Compte {lst_cpt[-1]} ajouté avec succès.")
+                    choix = int(input("Quelle fonctionnalité souhaitez-vous accéder ? : "))
                 case 3:
                     print("|-----Ajout d'opération-----|")
                     ajout_operation()
                     print(f"Opération : \n{lst_ope[-1]}\n ajoutée avec succès.")
+                    choix = int(input("Quelle fonctionnalité souhaitez-vous accéder ? : "))
                 case 4:
                     pass
                 case 5:
                     for elt in lst_ope:
                         print(elt)
-
-
+                    choix = int(input("Quelle fonctionnalité souhaitez-vous accéder ? : "))
 
 
 # --Programme principal--
